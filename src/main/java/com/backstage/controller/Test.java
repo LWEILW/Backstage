d16dfe1c256696800f19a42622a0bc4ce429fc23
LW <liu.wei@wuxi.stee.stengg.com.cn>
2020-08-06 10:25:23 +0800

    20200806更新

--- src/main/java/com/backstage/controller/Test.java
+++ src/main/java/com/backstage/controller/Test.java
@@ -4,13 +4,17 @@ public class Test {
 
 
     public static void main(String[] args) {
-        int x = 3;
-        show(x);
-        System.out.println(x);
+        int y = 3;
+        show(y);
+        System.out.println(y);
+        System.out.println(y);
+        System.out.println(y);
+        System.out.println(y);
+        System.out.println(y);
     }
 
-    public static void show(int x) {
-        x = 4;
-        System.out.println("a"+x);
+    public static void show(int y) {
+        y = 4;
+        System.out.println("a"+y);
     }
 }

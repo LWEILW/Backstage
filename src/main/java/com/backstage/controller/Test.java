7e6699ae10a23ee02708eefe7aa8aa8f844d2c43
LW <liu.wei@wuxi.stee.stengg.com.cn>
2020-08-06 10:42:05 +0800

    20200806更新

--- src/main/java/com/backstage/controller/Test.java
+++ src/main/java/com/backstage/controller/Test.java
@@ -5,6 +5,7 @@ public class Test {
 
     public static void main(String[] args) {
         int y = 3;
+        System.out.println(y);
         show(y);
         System.out.println(y);
         System.out.println(y);

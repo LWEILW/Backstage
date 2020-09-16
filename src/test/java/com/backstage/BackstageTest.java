a0e99a7447cedbef66b520aef2ba3d4f3bfed78c
LW <liu.wei@wuxi.stee.stengg.com.cn>
2020-09-16 14:39:28 +0800

    支线测试

---
+++ src/test/java/com/backstage/BackstageTest.java
@@ -0,0 +1,20 @@
+package com.backstage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackstageTest {
+
+
+    @Test
+    public void unitTesting() {
+//        Scanner scanner = new Scanner(System.in);
+        System.out.println("111");
+    }
+
+}
